64f13ba88a6a7029180da11796d45a6831b42ee4
arameharutyunyan32 <deffer555deffer@gmail.com>
2026-06-04 17:50:00 +0400

    car form

--- eslint.config.js
+++ eslint.config.js
@@ -52,6 +52,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
+      'react/require-default-props': 'off',
     },
     settings: {
       react: { version: 'detect' },
