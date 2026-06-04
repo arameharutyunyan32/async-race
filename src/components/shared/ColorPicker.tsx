f5b4d893ec6c8d9d75762a2e38eda82f67847c35
arameharutyunyan32 <deffer555deffer@gmail.com>
2026-06-04 16:35:00 +0400

    color picker

--- src/components/shared/ColorPicker.tsx
+++ src/components/shared/ColorPicker.tsx
@@ -1,4 +1,17 @@
-export default function ColorPicker() {
-  return <div>ColorPicker</div>;
+import styles from './ColorPicker.module.css';
+
+interface Props {
+  value: string;
+  onChange: (color: string) => void;
 }
 
+export default function ColorPicker({ value, onChange }: Props) {
+  return (
+    <input
+      type="color"
+      value={value}
+      onChange={(e) => { onChange(e.target.value); }}
+      className={styles.picker}
+    />
+  );
+}
